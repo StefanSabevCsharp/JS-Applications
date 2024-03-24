c000e77ca3be42ea5e6383a97d045526934bc842
Stefan Sabev <stevenbg91@gmail.com>
2024-03-24 07:42:10 +0100

    added trim to remove whitespaces

--- Movies-Single-Page-Applications/01. SoftForum_������/02.Movies/src/dataService/formHandler.js
+++ Movies-Single-Page-Applications/01. SoftForum_������/02.Movies/src/dataService/formHandler.js
@@ -4,7 +4,9 @@ export function createFormEntity(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     return [...formData.entries()].reduce((acc, [k, v]) => {
-        acc[k] = v;
+        //trim() removes white spaces from the input fields
+
+        acc[k] = v.trim();
         return acc;
     }, {});
 }
